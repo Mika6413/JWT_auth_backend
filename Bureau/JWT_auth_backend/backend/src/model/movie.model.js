8d19caeb8ced3b76b5ae67102bf1de398d9c7700
Mika6413 <mika6413@github.com>
2024-04-06 22:02:34 +0200

    backend à compléter ave SQLGenericError

---
+++ Bureau/JWT_auth_backend/backend/src/model/movie.model.js
@@ -0,0 +1,22 @@
+const db = require("src/model/db.js");
+const { SQLGenericError } = require("src/src/errors/SQLGenericError.js");
+const findAll = async () => {
+  try {
+    const [movies] = await db.query("SELECT* FROM `movie`");
+    return movies;
+  } catch (err) {
+    console.log(err);
+    throw new SQLGenericError();
+  }
+};
+
+const findOne = async () => {
+  try {
+    const [movie] = await db.query("SELECT * FROM `movie` WHERE id = ?", [id]);
+    return movie;
+  } catch (err) {
+    console.log(err);
+    throw new SQLGenericError();
+  }
+};
+module.exports = { findAll, findOne };
